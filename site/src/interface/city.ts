0e67e49124d222fb501cbd7a43b20cfc59cc54b3
Fernando Villalba <21fervillalbag@gmail.com>
2024-06-26 16:38:08 -0400

    fix: select barrio in demo view

--- site/src/interface/city.ts
+++ site/src/interface/city.ts
@@ -1,4 +1,13 @@
 export interface ICity {
+  data: ICityData[];
+  limit: number;
+  elements: number;
+  page: number;
+  sort: string;
+  total_pages: number;
+}
+
+export interface ICityData {
   id: number;
   nombre: string;
 }
--- site/src/interface/department.ts
+++ site/src/interface/department.ts
@@ -1,4 +1,13 @@
 export interface IDepartament {
+  data: IDataDepartment[];
+  limit: number;
+  elements: number;
+  page: number;
+  sort: string;
+  total_pages: number;
+}
+
+export interface IDataDepartment {
   id: number;
   nombre: string;
   gps: {
