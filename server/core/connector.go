8c50fe707a93b0e2ddbfc84731c3019528932a16
MacroHEX <martin18py@gmail.com>
2024-06-27 13:37:16 -0400

    fix: Fix env

--- server/core/connector.go
+++ server/core/connector.go
@@ -2,31 +2,45 @@ package core
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 	"os"
 )
 
 func ConnectToDb() *gorm.DB {
-	//err := godotenv.Load()
-	//
-	//if err != nil {
-	//	log.Fatal("Error al cargar el .env")
-	//}
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found. Continuing without it.")
+	}
 
+	// Retrieve environment variables
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASS")
+	sslMode := os.Getenv("DB_SSLMODE")
+	if sslMode == "" {
+		sslMode = "require" // default SSL mode
+	}
+	timeZone := os.Getenv("DB_TIMEZONE")
+	if timeZone == "" {
+		timeZone = "UTC" // default timezone
+	}
 
-	// ::: DB
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=require TimeZone=America/Asuncion", dbHost, dbPort, dbUser, dbPassword, dbName)
+	// Construct the Data Source Name (DSN)
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=%s",
+		dbHost, dbPort, dbUser, dbPassword, dbName, sslMode, timeZone,
+	)
 
+	// Connect to the database
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
-		panic("Conexión a la base de datos fallida: " + err.Error())
+		log.Fatal("Conexión a la base de datos fallida: ", err)
 	}
 
 	return db
--- server/main.go
+++ server/main.go
@@ -3,25 +3,37 @@ package main
 import (
 	"apiGo/api"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
 
 func main() {
-	// ::: ENV
-	//err := godotenv.Load()
-	//
-	//if err != nil {
-	//	log.Fatal("Error al cargar el .env")
-	//}
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found. Continuing without it.")
+	}
 
-	gin.SetMode(os.Getenv("GIN_MODE"))
+	// Set Gin mode based on environment variable, default to "release" mode if not set
+	ginMode := os.Getenv("GIN_MODE")
+	if ginMode == "" {
+		ginMode = "release"
+	}
+	gin.SetMode(ginMode)
 
-	// ::: HTTP SERVER :::
+	// Create a new Gin router
 	r := api.Router()
 
-	err = r.Run()
+	// Get the port from environment variables, default to 8080 if not set
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
+	// Run the Gin server on the specified port
+	err = r.Run(":" + port)
 	if err != nil {
-		panic(err)
+		log.Fatal("Failed to run server: ", err)
 	}
 }
